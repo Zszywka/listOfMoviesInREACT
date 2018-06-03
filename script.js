607993eca8c8416e7bf26d7d8a40af79bb6cb797
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-06-03 22:33:31 +0200

    Add style.css

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ var movies = [
   {
     id: 1,
     title: 'Groundhog Day',
-    desc: 'movie about time',
+    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: 'pictures/groundhog-day-films-photo-u13.webp',
     video: 'https://www.youtube.com/watch?v=tSVeDx9fk60'
   },
   {
     id: 2,
     title: 'The Lion King',
-    desc: 'movie about lions',
+    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: 'pictures/the-lion-king-films-photo-u15.webp',
     video: 'https://www.youtube.com/watch?v=4sj1MT05lAA'
   },
   {
     id: 3,
     title: 'The Shawshank Redemption',
-    desc: 'movie about prison',
+    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: 'pictures/the-shawshank-redemption-films-photo-u1.webp',
     video: 'https://www.youtube.com/watch?v=6hB3S9bIaco'
   },
   {
     id: 4,
     title: 'The Sixth Sense',
-    desc: 'movie about spirits',
+    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: 'pictures/the-sixth-sense-films-photo-u1.webp',
     video: 'https://www.youtube.com/watch?v=VG9AGf66tXM'
   }
@@ -34,13 +34,13 @@ var movieElements = movies.map(function(movie) {
     React.createElement('img', {src: movie.img}),
     React.createElement('h2', {}, movie.title),
     React.createElement('p', {}, movie.desc),
-    React.createElement('a', {href: movie.video}, 'triler' )
+    React.createElement('a', {href: movie.video}, 'Triler' )
   );
 });
 
 var element =
   React.createElement('div', {},
-    React.createElement('h1', {}, 'list of movies'),
+    React.createElement('h1', {}, 'The best of movies:'),
     React.createElement('ul', {}, movieElements)
 );
 
